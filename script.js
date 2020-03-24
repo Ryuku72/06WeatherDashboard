07ee16bca20c047a4823b8b4cc932e27c016d81a
Joshua K Bader <jaybshinsen@hotmail.com>
2020-03-25 02:05:05 +0800

    small issue with days

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ $(document).ready(function () {
 
           clear();
 
-          var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+          var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
           var d = new Date();
           var dayName = days[d.getDay()];
           //console.log(dayName);
